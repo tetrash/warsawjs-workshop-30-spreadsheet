beb14c742ce68ad985ce978472fd4b931fc48e4b
Piotrek Koszuliński <pkoszulinski@gmail.com>
2019-03-15 00:31:02 +0100

    WIP

--- src/data-provider.js
+++ src/data-provider.js
@@ -2,9 +2,30 @@ import dataSet from '../vendor/data-set-1000.json';
 
 export const AVAILABLE_COLUMNS = Object.keys( dataSet[ 0 ] );
 
+/**
+ * @param {Array.<String>} displayColumns Names of columns to display.
+ * @param {Number} complexityFactor How long should the formula computations take.
+ * @returns {DataSource}
+ */
 export function createDataProvider( displayColumns, complexityFactor ) {
+	/**
+	 * @typedef DataSource
+	 */
 	return {
+		/**
+		 * Number of columns to display.
+		 *
+		 * @type {Number}
+		 */
 		numberOfColumns: displayColumns.length,
+
+		/**
+		 * Returns the item value at the given position.
+		 *
+		 * @param {Number} row
+		 * @param {Number} col
+		 * @returns {*} This item value.
+		 */
 		getItem( row, col ) {
 			if ( row == 0 ) {
 				return displayColumns[ col ];
--- src/toby.js
+++ src/toby.js
@@ -6,18 +6,52 @@ const BORDER_WIDTH = 1;
 const PRELOAD_ROWS = 5;
 
 export default class Toby {
+	/**
+	 * @param {DataSource} dataSource
+	 */
 	constructor( dataSource ) {
+		/**
+		 * @type {DataSource}
+		 */
 		this.dataSource = dataSource;
 
+		/**
+		 * The first row which is currently in the DOM.
+		 *
+		 * @type {Number}
+		 */
 		this.firstRenderedRow = Number.MAX_SAFE_INTEGER;
+
+		/**
+		 * The last row which is currently in the DOM.
+		 *
+		 * @type {Number}
+		 */
 		this.lastRenderedRow = -1;
 
+		/**
+		 * The sentinel element – guards the scroll size by stretching the container.
+		 *
+		 * @type {Element}
+		 */
 		this._sentinel = this._createSentinelElement();
+
+		/**
+		 * The current sentinel's position from the top of the container.
+		 */
 		this._sentinelPosition = 0;
 
+		/**
+		 * The array of row elements which are currently in the DOM.
+		 *
+		 * @type {Array.<Element>}
+		 */
 		this._renderedRowElements = [];
 	}
 
+	/**
+	 * Attaches this toby instance to the given elements.
+	 */
 	attachTo( { container, statusBar } ) {
 		this.container = container;
 		this.cellStatusElement = statusBar.querySelector( '.status-bar__current-cell' );
@@ -29,27 +63,46 @@ export default class Toby {
 		observeScrollableViewport( container, () => this.render() );
 	}
 
+	/**
+	 * Detaches this toby instance.
+	 */
 	destroy() {
 		this.container.innerHTML = '';
 	}
 
+	/**
+	 * Updates the list of rows which should be rendered based on the current viewport position.
+	 */
 	render() {
 		const visibleRows = getVisibleRowRange( getViewport( this.container ) );
 
 		this._update( visibleRows );
 	}
 
+	/**
+	 * Renders missing rows and removes excessive rows.
+	 *
+	 * @param {Array.<Number>} rowsToRender Rows which should currently be rendered.
+	 */
 	_update( rowsToRender ) {
 		const firstRenderedRow = this.firstRenderedRow;
 		const lastRenderedRow = this.lastRenderedRow;
+
+		// The visible range of of rows extended with the given number of rows "beyond the fold".
+		// This is – there should always be a couple of rows outside of the viewport so the user
+		// doesn't see that they are being rendered as he/she scrolls.
 		const firstToRender = Math.max( 0, rowsToRender.first - PRELOAD_ROWS );
 		const lastToRender = Math.max( 0, rowsToRender.last + PRELOAD_ROWS );
 
+		// Update the rows from the first one which should be added or removed
+		// to the last one which should be added or removed.
 		const start = Math.min( firstRenderedRow, firstToRender );
 		const end = Math.max( lastRenderedRow, lastToRender );
 
 		for ( let row = start; row <= end; row++ ) {
 			if ( row >= firstToRender && row <= lastToRender ) {
+				// If the current row should be rendered (in the DOM) but isn't present in the
+				// table of rendered rows (wasn't rendered so far), render it.
 				if ( !this._renderedRowElements[ row ] ) {
 					const rowElement = this._renderRow( row );
 
@@ -58,6 +111,7 @@ export default class Toby {
 					this._renderedRowElements[ row ] = rowElement;
 				}
 			} else {
+				// If the current row should not be rendered but is currently rendered, remove it from the DOM.
 				if ( this._renderedRowElements[ row ] ) {
 					this._renderedRowElements[ row ].remove();
 					this._renderedRowElements[ row ] = null;
@@ -71,6 +125,12 @@ export default class Toby {
 		this._updateSentinel( lastToRender );
 	}
 
+	/**
+	 * Creates a row element and fills the cells inside it with the data of this row.
+	 *
+	 * @param {Number} row The number of the row to render.
+	 * @returns {Element} The rendered row element.
+	 */
 	_renderRow( row ) {
 		const rowElement = this._createRowElement( row );
 
@@ -83,20 +143,12 @@ export default class Toby {
 		return rowElement;
 	}
 
-	_updateSentinel( lastRow ) {
-		const stretchToRow = lastRow + 5;
-
-		if ( stretchToRow > this._sentinelPosition ) {
-			// Theoretically, setting transform should work fine, but
-			// it didn't work in some (random) cases. Seemed to be a Blink's bug.
-			// this._sentinel.style.transform = `translateY(${ stretchToRow * ( ROW_HEIGHT + BORDER_WIDTH ) }px)`;
-
-			this._sentinel.style.top = `${ stretchToRow * ( ROW_HEIGHT + BORDER_WIDTH ) }px`;
-
-			this._sentinelPosition = stretchToRow;
-		}
-	}
-
+	/**
+	 * Creates a row element and all its cells.
+	 *
+	 * @param {Number} row The number of the row to create.
+	 * @returns {Element} The rendered row element (with empty cells).
+	 */
 	_createRowElement( row ) {
 		const rowElement = document.createElement( 'div' );
 
@@ -111,6 +163,12 @@ export default class Toby {
 		return rowElement;
 	}
 
+	/**
+	 * Creates a cell element.
+	 *
+	 * @param {Number} row The number of the row to render
+	 * @returns {Element} The rendered row element (with empty cells).
+	 */
 	_createCellElement( col ) {
 		const cellElement = document.createElement( 'div' );
 
@@ -151,10 +209,31 @@ export default class Toby {
 
 		this.cellStatusElement.innerHTML = `row: ${ row }, col: ${ col }, data: ${ this.dataSource.getItem( row, col ) }`;
 	}
+
+	/**
+	 * Updates the sentinel's position to be 5 rows below the last rendered row.
+	 * Thanks to that there's always space for the scroll to move.
+	 */
+	_updateSentinel( lastRow ) {
+		const stretchToRow = lastRow + 5;
+
+		if ( stretchToRow > this._sentinelPosition ) {
+			// Theoretically, setting transform should work fine, but
+			// it didn't work in some (random) cases. Seemed to be a Blink's bug.
+			// this._sentinel.style.transform = `translateY(${ stretchToRow * ( ROW_HEIGHT + BORDER_WIDTH ) }px)`;
+
+			this._sentinel.style.top = `${ stretchToRow * ( ROW_HEIGHT + BORDER_WIDTH ) }px`;
+
+			this._sentinelPosition = stretchToRow;
+		}
+	}
 }
 
 /**
+ * Observes the given scrollable element for changes in its viewport (the visible part of its content).
+ *
  * @param {Element} container Scrollable container.
+ * @param {Function} callback The function to call when the viewport changes.
  */
 function observeScrollableViewport( container, callback ) {
 	container.addEventListener( 'scroll', onChange );
@@ -165,6 +244,16 @@ function observeScrollableViewport( container, callback ) {
 	}
 }
 
+/**
+ * Gets the viewport position (the visible part of the given container element's content).
+ *
+ *		// The below means that the visible part of the element
+ *		// starts at position 100px from its top to 500px from its top.
+ *		getViewport( container ); // -> { top: 100, bottom: 500 }
+ *
+ * @param {Element} container Scrollable container.
+ * @returns {Object} The viewport position (its top and bottom positions in pixels).
+ */
 function getViewport( container ) {
 	return {
 		top: container.scrollTop,
@@ -172,6 +261,12 @@ function getViewport( container ) {
 	};
 }
 
+/**
+ * Calculates the first and last rows visible in the given viewport.
+ *
+ * @param {Object} viewport
+ * @returns {OBject} The first and last visible rows.
+ */
 function getVisibleRowRange( viewport ) {
 	const fullRowHeight = ROW_HEIGHT + BORDER_WIDTH;
 
